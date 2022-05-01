43508dd0f880805703edf2a8ca5bacd98bbb1c2d
Anupama CV <anupamac@bipp.io>
2022-05-01 19:52:25 +0530

    Added: functionality to get component with dynamic data

--- expense-tracker-app/src/App.js
+++ expense-tracker-app/src/App.js
@@ -5,6 +5,8 @@ function App() {
     <div className="App">
       <h1>Let's get started!</h1>
       <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
--- expense-tracker-app/src/components/ExpenseItem.js
+++ expense-tracker-app/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+    const expenseDate = new Date(2022, 2, 28);
+    const expenseTitle = 'Car Insurance '
+    const expenseAmount = 100000;
+
     return ( 
         <div className='expense-item'>
-            <div>March 11th 2022</div>
+            <div>{expenseDate.toISOString}</div>
             <div className='expense-item__description'>
-                <h2>Car Insurance</h2>
-                <div className='expense-item__price'>100000K</div>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>{expenseAmount}</div>
             </div>    
         </div>    
     );
