9557e81c995924040eace3b6d3832febb672057e
Anupama CV <anupamac@bipp.io>
2022-05-02 12:10:04 +0530

    Added: functionalty to split components into multiple components

--- expense-tracker-app/src/components/ExpenseItem.js
+++ expense-tracker-app/src/components/ExpenseItem.js
@@ -1,22 +1,16 @@
-import './ExpenseItem.css'
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 
 function ExpenseItem(props) {
-    const month = props.date.toLocaleString('en-US',{month: 'long'});
-    const year = props.date.toLocaleString('en-US',{day: '2-digit'});
-    const day = props.date.getFullYear();
-
     return ( 
-        <div className='expense-item'>
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date}></ExpenseDate>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount}</div>
             </div>    
-        </div>    
+        </Card>    
     );
 }
 
